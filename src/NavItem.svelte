1ffa886530fda05ad5aa41a67895f4bbaef9727c
JackFly26 <jackfleishman@outlook.com>
2020-01-05 09:41:50 -0500

    initial commit

---
+++ src/NavItem.svelte
@@ -0,0 +1,23 @@
+<style>
+	span {
+		background-color: #800000;
+		flex-grow: 1;
+		color: white;
+		text-align: center;
+		line-height: 3rem;
+		cursor: pointer;
+		font-size: 1.5rem;
+	}
+	span:hover {
+		background-color: #640000
+	}
+</style>
+<script>
+	export let text;
+	export let setPage;
+</script>
+<span on:click={
+			setPage(text.toLowerCase())
+			}>
+	{text}
+</span>
