8b47ee9e747f5979107d376e3b7cfc5e02fb139c
JackFly26 <jackfleishman@outlook.com>
2020-01-05 21:34:51 -0500

    add cards (need pics of members}

--- src/About.svelte
+++ src/About.svelte
@@ -1,9 +1,35 @@
+<script>
+ import Card from './Card.svelte'
+ const bios = [
+     {
+	 src: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.9FZZxs6TXZPSNRQyY0d-FAHaFj%26pid%3DApi&f=1",
+	 name: "Jack",
+	 description: "Bassist"
+     },
+     {
+	 src: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.9FZZxs6TXZPSNRQyY0d-FAHaFj%26pid%3DApi&f=1",
+	 name: "Ethan",
+	 description: "Drummer/Trombonist"
+     },
+     {
+	 src: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.9FZZxs6TXZPSNRQyY0d-FAHaFj%26pid%3DApi&f=1",
+	 name: "Patrick",
+	 description: "Drummer/???"
+     }
+ ]
+</script>
+<style>
+ .cards {
+     display: flex;
+ }
+</style>
 <h1>
     About Us
 </h1>
 <p>Hi! We are The G, a band based in Atlanta, Georgia. We make modern jazz fusion style pieces to perform and share online. We love making and playing music, and want to share our creations with everyone. Enjoy!</p>
 <h3>Members:</h3>
-
-<p>Jack - Bass</p>
-<p>Ethan -Drumset/Trombone</p>
-<p>Patrick - Drumset/???</p>
+<div class="cards">
+    {#each bios as member}
+	<Card {...member}/>
+    {/each}
+</div>
---
+++ src/Card.svelte
@@ -0,0 +1,31 @@
+<script>
+ export let name;
+ export let src;
+ export let description;
+</script>
+<style>
+ .card {
+     flex-grow: 1;
+     background-color: rgb(45, 49, 51);
+     color: white;
+     padding: 1rem;
+     margin: 1rem;
+     transition: .25s ease all;
+     border: 4px #640000 solid;
+ }
+ .card:hover {
+     border: 4px #d00000 solid;
+ }
+ img {
+     width: 100%;
+ }
+</style>
+<div class="card">
+    <img {src} alt={name}>
+    <h3>
+	{name}
+    </h3>
+    <p>
+	{description}
+    </p>
+</div>
