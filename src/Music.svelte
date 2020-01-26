7fd2e7766bdf044fc6534cb85403a0cb952a1ec8
JackFly26 <jackfleishman@outlook.com>
2020-01-25 20:44:42 -0500

    phones are cool too

--- src/Music.svelte
+++ src/Music.svelte
@@ -3,19 +3,41 @@
      display: flex;
      flex-wrap: wrap;
      justify-content: space-evenly;
+     align-content: space-around;
  }
 
- iframe {
+ .soundcloud {
      flex-basis: 400px;
  }
 
+ iframe {
+     width: 100%;
+ }
+
  @media only screen and (max-width: 500px) {
-     iframe {
+     .soundcloud {
 	 flex-grow: 1;
 	 flex-basis: auto;
+	 padding: 1rem 0;
      }
  }
 </style>
+<script>
+ const songs = [
+     {
+	 title: "Initiation",
+	 src: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/749475481&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+     },
+     {
+	 title: "EB2",
+	 src: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/749475802&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+     },
+     {
+	 title: "Sunset",
+	 src: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/749476138&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+     }
+ ]
+</script>
 <h1>
     Our Music
 </h1>
@@ -24,11 +46,11 @@
 <p>Demos of our songs.</p>
 
 <div class="flex-container">
-<iframe style="margin-right: 15px;" title="Initiation" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/749475481&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-
-<iframe style="margin-right: 15px;" title="EB2" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/749475802&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-
-<iframe style="margin-right: 15px;" title="Sunset" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/749476138&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+    {#each songs as song}
+	<div class="soundcloud">
+	    <iframe title="Initiation" height="300" scrolling="no" frameborder="no" allow="autoplay" {...song}></iframe>
+	</div>
+    {/each}
 </div>
 
 <h2>Scores</h2>
