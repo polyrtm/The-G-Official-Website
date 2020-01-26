74e5283fca9715d7c4c33dddb056ce30d379f4b0
JackFly26 <jackfleishman@outlook.com>
2020-01-25 20:13:08 -0500

    kinda works

--- src/Music.svelte
+++ src/Music.svelte
@@ -1,3 +1,21 @@
+<style>
+ .flex-container {
+     display: flex;
+     flex-wrap: wrap;
+     justify-content: space-evenly;
+ }
+
+ iframe {
+     flex-basis: 400px;
+ }
+
+ @media only screen and (max-width: 500px) {
+     iframe {
+	 flex-grow: 1;
+	 flex-basis: auto;
+     }
+ }
+</style>
 <h1>
     Our Music
 </h1>
@@ -5,11 +23,13 @@
 <h2>Demos</h2>
 <p>Demos of our songs.</p>
 
+<div class="flex-container">
 <iframe style="margin-right: 15px;" title="Initiation" width="400" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/749475481&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
 
 <iframe style="margin-right: 15px;" title="EB2" width="400" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/749475802&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
 
 <iframe style="margin-right: 15px;" title="Sunset" width="400" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/749476138&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+</div>
 
 <h2>Scores</h2>
 <p>Coming Soon</p>
